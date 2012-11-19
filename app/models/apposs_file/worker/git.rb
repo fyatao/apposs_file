53facaea77ff052f38da3a08ee3080fc28578f64
fsword <li.jianye@gmail.com>
2012-11-19 22:24:34 +0800

    界面调整；定义 download_url

--- app/models/apposs_file/worker/git.rb
+++ app/models/apposs_file/worker/git.rb
@@ -1,18 +1,18 @@
 module AppossFile
   module Worker
     module Git
-      class << self
-        def init_scripts refer_url
-          [ "git clone #{refer_url} target" ]
-        end
+      extend self
+      
+      def init_scripts refer_url
+        [ "git clone #{refer_url} target" ]
+      end
 
-        def pre_scripts refer_url, download_folder
-          [
-            "rm -rf #{download_folder}/target.tgz",
-            "cd target && git fetch origin && git reset --hard origin/master",
-            "cd target && tar cz . --exclude=.git -f #{download_folder}/target.tgz",
-          ]
-        end
+      def pre_scripts refer_url, download_folder
+        [
+          "rm -rf #{download_folder}/target.tgz",
+          "cd target && git fetch origin && git reset --hard origin/master",
+          "cd target && tar cz . --exclude=.git -f #{download_folder}/target.tgz",
+        ]
       end
     end
   end
--- app/models/apposs_file/worker/svn.rb
+++ app/models/apposs_file/worker/svn.rb
@@ -1,18 +1,16 @@
 module AppossFile
   module Worker
     module Svn
-      class << self
+      extend self
 
-        def pre_scripts refer_url, download_folder
-          [
-            "rm -rf target",
-            "rm -rf #{download_folder}/target.tgz",
-            "svn export #{refer_url} target",
-            "cd target && tar cz . -f #{download_folder}/target.tgz",
-            "rm -rf target"
-          ]
-        end
-
+      def pre_scripts refer_url, download_folder
+        [
+          "rm -rf target",
+          "rm -rf #{download_folder}/target.tgz",
+          "svn export #{refer_url} target",
+          "cd target && tar cz . -f #{download_folder}/target.tgz",
+          "rm -rf target"
+        ]
       end
     end
   end
